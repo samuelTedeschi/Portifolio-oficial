46bb2b084099568acd4814e5f1e2049347d009c8
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-03-10 11:10:36 -0300

    scrollbar editado

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
-      <body>
+      <body className="scrollbar scrollbar-thumb-yellow-600 scrollbar-thin">
         <Main />
         <NextScript />
       </body>
